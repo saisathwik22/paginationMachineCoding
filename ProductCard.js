6837f0f0d9bd0a1d0a7ad08fcccc49840951a3ca
Sai Sathwik Reddy <150815533+saisathwik22@users.noreply.github.com>
2025-05-03 08:55:22 +0530

    Create ProductCard.js

---
+++ ProductCard.js
@@ -0,0 +1,8 @@
+export default ProductCard = ({ image, title }) => {
+  return (
+    <div className="product-card">
+      <img src={image} alt={title} className="product-img" />
+      <span>{title}</span>
+    </div>
+  );
+};
